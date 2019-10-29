dd76d982c95a665cf61fa4a46bd71db4f1ac6d73
petitprec <celine.petitpre.etu@univ-lille.fr>
2019-10-29 13:57:40 +0100

    ajout des fichiers ipd.py et strategies.py

---
+++ strategies.py
@@ -0,0 +1,193 @@
+import itertools
+import random
+
+class Strategy():
+    def setMemory(self,mem):
+        pass
+    
+    def getAction(self,tick):
+        pass
+    
+    def __copy__(self):
+        pass
+
+    def update(self,x,y):
+        pass
+    
+
+class Periodic(Strategy):
+    def __init__(self, sequence, name=None):
+        super().__init__()
+        self.sequence = sequence.upper()
+        self.step = 0
+        self.name = "per_"+sequence if (name == None) else name
+
+    def getAction(self,tick):
+        return self.sequence[tick % len(self.sequence)]
+
+    def clone(self):
+        object = Periodic(self.sequence, self.name)
+        return object
+
+    def update(self,x,y):
+        pass
+
+class Tft(Strategy):
+    def __init__(self):
+        super().__init__()
+        self.name = "tft"
+        self.hisPast=""
+        
+    def getAction(self,tick):
+        return 'C' if (tick==0) else self.hisPast[-1]
+
+    def clone(self):
+        return Tft()
+
+    def update(self,my,his):
+        self.hisPast+=his
+    
+    
+class Spiteful(Strategy):
+    def __init__(self):
+        super().__init__()
+        self.name = "spiteful"
+        self.hisPast=""
+        self.myPast=""
+        
+    def getAction(self,tick):
+        if (tick==0):
+                return 'C'
+        if (self.hisPast[-1]=='D' or self.myPast[-1]=='D') :
+            return 'D'
+        else :
+            return 'C'
+
+    def clone(self):
+        return Spiteful()
+
+    def update(self,my,his):
+        self.myPast+=my
+        self.hisPast+=his
+
+class SoftMajority(Strategy):
+    def __init__(self):
+        super().__init__()
+        self.name = "softmajo"
+        self.nbCooperations = 0
+        self.nbTrahisons = 0
+        
+    def getAction(self,tick):
+        if (self.nbCooperations >= self.nbTrahisons):
+            return 'C'
+        else :
+            return 'D'
+
+    def clone(self):
+        return SoftMajority()
+
+    def update(self,my,his):
+        if (his == 'C'):
+            self.nbCooperations += 1
+        elif (his == 'D'):
+            self.nbTrahisons += 1
+
+class HardMajority(Strategy):
+    def __init__(self):
+        super().__init__()
+        self.name = "hardmajo"
+        self.nbCooperations = 0
+        self.nbTrahisons = 0
+        
+    def getAction(self,tick):
+        if (self.nbCooperations > self.nbTrahisons):
+            return 'C'
+        else :
+            return 'D'
+
+    def clone(self):
+        return HardMajority()
+
+    def update(self,my,his):
+        if (his == 'C'):
+            self.nbCooperations += 1
+        elif (his == 'D'):
+            self.nbTrahisons += 1
+            
+
+class Gradual(Strategy):
+    def __init__(self):
+        super().__init__()
+        self.name = "gradual"
+        self.nbTrahisons = 0
+        self.punish = 0
+        self.calm = 0
+    def getAction(self,tick):
+        if (tick==0) : return 'C'
+        if self.punish > 0 :
+            self.punish-=1
+            return 'D'
+        if self.calm > 0 :
+            self.calm-=1
+            return 'C'
+        if self.hisLast=='D' : 
+            self.punish=self.nbTrahisons - 1
+            self.calm=2
+            return 'D'
+        else: return 'C'
+
+    def clone(self):
+        return Gradual()
+
+    def update(self,my,his):
+        self.hisLast=his
+        if (his == 'D'):
+            self.nbTrahisons += 1
+
+class Mem(Strategy):
+    def __init__(self, x, y, genome, name=None):
+        self.name = name
+        self.x = x
+        self.y = y
+        self.genome = genome
+        if (name == None): #Nom par défaut si l'utilisateur ne le définit pas
+            self.name = genome
+        self.myMoves = [] #contains my x last moves
+        self.itsMoves = [] #contains its y last moves
+
+    def clone(self):
+        return Mem(self.x, self.y, self.genome, self.name)
+
+    def getAction(self, tick):
+        if (tick < max(self.x, self.y)):
+            return self.genome[tick]
+        cpt = 0
+        for i in range(self.x-1,-1,-1):
+            cpt*=2
+            if (self.myMoves[i] == 'D'):
+                cpt+=1
+        for i in range(self.y-1,-1,-1):
+            cpt*=2
+            if (self.itsMoves[i] == 'D'):
+                cpt+=1
+        cpt += max(self.x, self.y)
+        return self.genome[cpt]
+
+    def update(self, myMove, itsMove):
+        if (self.x > 0):
+            if(len(self.myMoves) == self.x):
+                del self.myMoves[0]
+            self.myMoves.append(myMove)
+        if (self.y > 0):
+            if(len(self.itsMoves) == self.y):
+                del self.itsMoves[0]
+            self.itsMoves.append(itsMove)
+
+def getAllMemory(x,y):
+    if (x+y > 4):
+        return "Pas calculable"
+    len_genome = max(x,y)+2**(x+y)
+    permut = [p for p in itertools.product(['C','D'], repeat=len_genome)]
+    genomes = [''.join(p) for p in permut]
+    return [Mem(x,y,gen) for gen in genomes]
+
